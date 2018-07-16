30f24446b2eec608898c55a89c79f7d40341c05f
Bill Sheng <Bill.Sheng@CORP.AD.CTC>
2018-07-16 10:15:26 -0400

    navbar responsive

--- frontend/app/core/header/header.js
+++ frontend/app/core/header/header.js
@@ -1,24 +1,30 @@
 $(document).ready(function () {
-  $(this).scrollTop(0);
+
+
   var primaryNav = $('.nav-primary');
 
+  var waypoint = $('.container-waypoint');
+
   new Waypoint({
-    element: primaryNav,
+    element: waypoint,
     handler: function (direction) {
       toggleNav();
-    }
+    }, offset: 109
 
-  }, {
-    offset: '8px'
   })
 
 
   function toggleNav() {
-    primaryNav.toggleClass(['navbar-dark', 'navbar-light', 'bg-dark', 'bg-light', 'loaded'])
+    primaryNav.toggleClass(['navbar-dark', 'navbar-light', 'bg-dark', 'bg-light', 'loaded', 'sticky-top'])
 
   };
 
-
+var scroll = 0;
+$(window).on('scroll', function() {
+  primaryNav.toggleClass('scroll-hidden', $(window).scrollTop() > scroll);
+  //if second parameter is true, class is added, else removed
+  scroll = $(window).scrollTop();
+})
 
 
 
