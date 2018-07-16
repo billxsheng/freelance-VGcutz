bc086a15d1a0fec84750a5c964c77c77f4c180b1
Bill Sheng <Bill.Sheng@CORP.AD.CTC>
2018-07-16 13:57:51 -0400

    header

--- frontend/app/core/header/header.js
+++ frontend/app/core/header/header.js
@@ -1,30 +1,57 @@
 $(document).ready(function () {
 
-
   var primaryNav = $('.nav-primary');
-
+  var mobileNav = $('.nav-mobile');
+  var mobileNavActivateBtn = $('.nav-mobile-btn');
   var waypoint = $('.container-waypoint');
+  var mobileNavLinks = $('.nav-link-mobile')
+  var scroll = 0;
 
-  new Waypoint({
-    element: waypoint,
-    handler: function (direction) {
-      toggleNav();
-    }, offset: 109
 
+  $(function () {
+    new Waypoint({
+      element: waypoint,
+      handler: function (direction) {
+        toggleNav();
+      }, offset: 109
+    })
   })
 
+  $(function () {
+    mobileNavActivateBtn.on('click', function () {
+      mobileNav.toggleClass('load-nav-content')
+    });
+
+  });
+  $(function () {
+    $(window).on('scroll', function () {
+      primaryNav.toggleClass('scroll-hidden', $(window).scrollTop() > scroll);
+      //if second parameter is true, class is added, else removed
+      scroll = $(window).scrollTop();
+    })
+  });
+
+
+  $(function () {
+    mobileNavLinks.on('click', function () {
+      mobileNav.toggleClass('load-nav-content');
+    });
+  })
 
   function toggleNav() {
     primaryNav.toggleClass(['navbar-dark', 'navbar-light', 'bg-dark', 'bg-light', 'loaded', 'sticky-top'])
 
   };
 
-var scroll = 0;
-$(window).on('scroll', function() {
-  primaryNav.toggleClass('scroll-hidden', $(window).scrollTop() > scroll);
-  //if second parameter is true, class is added, else removed
-  scroll = $(window).scrollTop();
-})
+
+
+
+
+
+
+
+
+
 
 
 
