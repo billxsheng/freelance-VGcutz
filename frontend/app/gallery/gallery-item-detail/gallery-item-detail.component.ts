4815ac15a12ab8a0fa3678f3c02b08aad22738bf
Bill Sheng <billxsheng@gmail.com>
2018-07-19 20:19:49 -0400

    service implemented, subscribe, route parameters

--- frontend/app/gallery/gallery-item-detail/gallery-item-detail.component.ts
+++ frontend/app/gallery/gallery-item-detail/gallery-item-detail.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "../../../../node_modules/@angular/core";
+import { Component, OnInit } from "../../../../node_modules/@angular/core";
+import { GalleryService } from "../gallery.service";
+import { ActivatedRoute, Params } from "../../../../node_modules/@angular/router";
+import { GalleryItem } from "../gallery-item.model";
 
 @Component({
     templateUrl: './gallery-item-detail.component.html',
     styleUrls: ['./gallery-item-detail.component.css']
 })
 
-export class GalleryItemDetailComponent {
-    
+export class GalleryItemDetailComponent implements OnInit {
+    id:Number;
+    selectedItem : GalleryItem;
+    constructor(private galleryService: GalleryService, private route: ActivatedRoute) {
+
+    }
+
+    ngOnInit() {
+        this.route.params.subscribe(
+            (params: Params) => {
+                this.id = +params['id'];
+                this.selectedItem = this.galleryService.getItem(this.id);
+            }
+        );
+    }
 }
--- frontend/app/gallery/gallery.service.ts
+++ frontend/app/gallery/gallery.service.ts
@@ -3,11 +3,18 @@ import { GalleryItem } from "./gallery-item.model";
 export class GalleryService {
 
     private galleryItems: GalleryItem[] = [
-        new GalleryItem('fade', 'fade', ''),
-        new GalleryItem('clean', 'clean',"")
+        new GalleryItem('fade', 'fade', "http://localhost:8080/frontend/assets/photos/gallery/monkey.jpg"),
+        new GalleryItem('clean', 'clean', "http://localhost:8080/frontend/assets/photos/gallery/monkey.jpg"),
+        new GalleryItem('clean', 'clean', "http://localhost:8080/frontend/assets/photos/gallery/monkey.jpg"),
+        new GalleryItem('clean', 'clean', "http://localhost:8080/frontend/assets/photos/gallery/monkey.jpg")
+
     ];
 
     getGalleryItems() {
         return this.galleryItems.slice();
+    }
+
+    getItem(id) {
+        return this.galleryItems[id];
     }
 }
